aeda14104a1619da98ecd3a71800b2fd6b8b9cec
Maicon R. Silva <maiconrods@gmail.com>
2020-04-23 00:04:53 -0300

    feat: added middlewares concept to the application

--- nivel-01/nodejs/aulas/src/index.js
+++ nivel-01/nodejs/aulas/src/index.js
@@ -1,11 +1,36 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+function logRequests(req, res, next) {
+  const { method, url } = req;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid project id.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (req, res) => {
   const { owner } = req.query;
