ae88569caf602cdb1ea1aea4ad1ea13f3a1dfdb0
Maicon R. Silva <maiconrods@gmail.com>
2020-04-22 22:03:05 -0300

    feat: Added new routes

--- nivel-01/nodejs/aulas/src/index.js
+++ nivel-01/nodejs/aulas/src/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ ok: false });
+app.get('/projects', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2'
+  ]);
+});
+
+app.post('/projects', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ])
+});
+
+app.put('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 2',
+    'Projeto 3'
+  ]);
 });
 
 app.listen(2408, () => {
