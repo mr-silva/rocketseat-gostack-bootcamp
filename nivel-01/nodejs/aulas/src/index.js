524fe0ff61eaa4421ee958bf1a793cb0d42af43e
Maicon R. Silva <maiconrods@gmail.com>
2020-04-22 22:19:47 -0300

    feat: added params to routes

--- nivel-01/nodejs/aulas/src/index.js
+++ nivel-01/nodejs/aulas/src/index.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (req, res) => {
+  const query = req.query;
+  const { title, owner } = req.query;
+
+  console.log(query);
+  console.log(title);
+  console.log(owner);
+
   return res.json([
     'Projeto 1',
     'Projeto 2'
@@ -10,6 +19,13 @@ app.get('/projects', (req, res) => {
 });
 
 app.post('/projects', (req, res) => {
+  const body = req.body;
+  const { title, owner } = req.body;
+
+  console.log(body);
+  console.log(title);
+  console.log(owner);
+
   return res.json([
     'Projeto 1',
     'Projeto 2',
@@ -18,6 +34,12 @@ app.post('/projects', (req, res) => {
 });
 
 app.put('/projects/:id', (req, res) => {
+  const params = req.params;
+  const { id } = req.params;
+
+  console.log(params);
+  console.log(id);
+
   return res.json([
     'Projeto 4',
     'Projeto 2',
