0692a87509f9638ea6d7b3b9e2075bad499653ad
Maicon R. Silva <maiconrods@gmail.com>
2020-04-23 20:52:28 -0300

    Added props to Header component

--- nivel-01/reactjs/src/App.js
+++ nivel-01/reactjs/src/App.js
@@ -5,9 +5,19 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header />
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Projetos</li>
+          <li>Links</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Novos Projetos</li>
+          <li>Destaques do mês</li>
+          <li>FAQ</li>
+        </ul>
+      </Header>
     </>
   );
 }
