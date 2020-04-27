023a4c35725760a73320a1cdc189f98812d58385
Maicon R. Silva <maiconrods@gmail.com>
2020-04-27 18:41:51 -0300

    Added eslint and prettier configs

--- nivel-02/nodejs/src/server.ts
+++ nivel-02/nodejs/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ ok: 'Hellow meuamigo' });
-});
+app.use(routes);
 
 app.listen(2408, () => {
-  console.log('Server started on port 2408')
+  console.log('Server started on port 2408');
 });
