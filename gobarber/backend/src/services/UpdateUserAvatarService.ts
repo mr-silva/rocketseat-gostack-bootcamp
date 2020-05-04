7c575d780b9f08c2677e6007bc7fa87c7fad922c
Maicon R. Silva <maiconrods@gmail.com>
2020-05-04 00:03:51 -0300

    (refactor) Treatment of Errors: Added a global error treatment, moved the status returns and messages formats to a middleware

--- gobarber/backend/src/services/UpdateUserAvatarService.ts
+++ gobarber/backend/src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 
 import User from '../models/User';
 import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
 
 interface Request {
   userId: string;
@@ -17,7 +18,7 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(userId);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar.');
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
     if (user.avatar) {
