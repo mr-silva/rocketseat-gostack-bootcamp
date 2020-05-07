4db4bc71a15cd776e6ffc7d61a68286944f3561a
Maicon R. Silva <maiconrods@gmail.com>
2020-05-07 15:25:05 -0300

    (style) Dashboard Style: created a style for the dashboard component.

--- git-explorer/src/pages/Dashboard/index.tsx
+++ git-explorer/src/pages/Dashboard/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/52940839?s=460&v=4"
+            alt="Maicon R. Silva"
+          />
+          <div>
+            <strong>mr-silva/rocketseat-gostack-bootcamp</strong>
+            <p>
+              Repository to store all challenges and activites from the GoStack
+              Bootcamp
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
