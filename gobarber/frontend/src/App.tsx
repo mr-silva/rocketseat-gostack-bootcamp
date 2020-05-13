fc44dbfaac2f154df8d624aed483cf35739a57f5
Maicon R. Silva <maiconrods@gmail.com>
2020-05-13 14:27:28 -0300

    (feature) Toast Messages: added toast messages to application, that can be displayed on error, success or info.

--- gobarber/frontend/src/App.tsx
+++ gobarber/frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn/index';
-// import SignUp from './pages/SignUp/index';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 
-import { AuthProvider } from './context/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
       {/* <SignUp /> */}
-    </AuthProvider>
+    </AppProvider>
+
     <GlobalStyle />
   </>
 );
