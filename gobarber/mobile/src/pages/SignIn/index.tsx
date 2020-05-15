683e194650389d5bcea7e0878f5a2ef05b852de4
Maicon R. Silva <maiconrods@gmail.com>
2020-05-15 01:30:21 -0300

    (feature) Sign In Route: completed the style of the Sign In route.

--- gobarber/mobile/src/pages/SignIn/index.tsx
+++ gobarber/mobile/src/pages/SignIn/index.tsx
@@ -1,32 +1,75 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+  Icon,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <>
-      <Container>
-        <Image source={logoImg} />
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" enabled>
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
 
-        <Title>Faça seu logon</Title>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-        <Input name="email" icon="mail" placeholder="E-mail" />
-        <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-        <Button
+            <Button
+              onPress={() => {
+                console.log('button pressed');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword
+              onPress={() => {
+                console.log('button pressed');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <KeyboardAvoidingView enabled={false}>
+        <CreateAccountButton
           onPress={() => {
             console.log('button pressed');
           }}
         >
-          Entrar
-        </Button>
-      </Container>
+          <Icon name="log-in" size={20} />
+          <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      </KeyboardAvoidingView>
     </>
   );
 };
