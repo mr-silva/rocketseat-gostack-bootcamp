7149bd04cca2d3a440e81d53100f82073caaf6d7
Maicon R. Silva <maiconrods@gmail.com>
2020-05-20 17:56:08 -0300

    (tests) Create Appointment: created test to Create Appointment Service.

---
+++ gobarber/backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,39 @@
+import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment =>
+        isEqual(appointment.date, date) &&
+        appointment.provider_id === provider_id,
+    );
+
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), provider_id, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
---
+++ gobarber/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,64 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123');
+  });
+
+  it('should not be able to create a new appointment when date is not available', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 22, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create appointment in the same date for diferent provider', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 22, 11);
+
+    const appointmentWithProvider123 = await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123',
+    });
+
+    const appointmentWithProvider1234 = await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '1234',
+    });
+
+    expect(appointmentWithProvider123).toHaveProperty('id');
+    expect(appointmentWithProvider1234).toHaveProperty('id');
+  });
+});
