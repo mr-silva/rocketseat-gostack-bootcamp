a382719c84a5ddbef895307b042b8d33118106fc
Maicon R. Silva <maiconrods@gmail.com>
2020-05-21 12:28:58 -0300

    (tests) Update User Avatar: created tests to Update User Avatar.

---
+++ gobarber/backend/src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,78 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeStorageProvider from '@shared/container/providers/StorageProviders/fakes/FakeStorageProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateUserAvatar from './UpdateUserAvatarService';
+
+describe('UpdateUserAvatar', () => {
+  it('should be able to add a avatar to user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatar(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'Test',
+      email: 'test@email.com',
+      password: '123456',
+    });
+
+    await updateUserAvatar.execute({
+      userId: user.id,
+      avatarFilename: 'test.png',
+    });
+
+    expect(user.avatar).toBe('test.png');
+  });
+
+  it('should not be able to update unauthenticated/unexisting user avatar', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatar(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatar.execute({
+        userId: 'unexistent-user',
+        avatarFilename: 'test.png',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able delete old avatar when updating to a new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const updateUserAvatar = new UpdateUserAvatar(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'Test',
+      email: 'test@email.com',
+      password: '123456',
+    });
+
+    await updateUserAvatar.execute({
+      userId: user.id,
+      avatarFilename: 'old-avatar.png',
+    });
+
+    await updateUserAvatar.execute({
+      userId: user.id,
+      avatarFilename: 'new-avatar.png',
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('old-avatar.png');
+    expect(user.avatar).toBe('new-avatar.png');
+  });
+});
--- gobarber/backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ gobarber/backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,12 +1,10 @@
-import path from 'path';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import uploadConfig from '@config/upload';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProviders/models/IStorageProvider';
 
 interface IRequest {
   userId: string;
@@ -18,6 +16,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ userId, avatarFilename }: IRequest): Promise<User> {
@@ -28,15 +29,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.update(user);
 
