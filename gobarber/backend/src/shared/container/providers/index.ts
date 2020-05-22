0461cf945254e680464c982a30cf2789bd101dba
Maicon R. Silva <maiconrods@gmail.com>
2020-05-22 20:31:41 -0300

    (feature) Mail Template: created a mail template provider.

--- gobarber/backend/src/shared/container/providers/index.ts
+++ gobarber/backend/src/shared/container/providers/index.ts
@@ -2,9 +2,11 @@ import { container } from 'tsyringe';
 
 import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlearsMailTemplateProvider';
 
 import IStorageProvider from './StorageProviders/models/IStorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -14,4 +16,9 @@ container.registerSingleton<IStorageProvider>(
 container.registerInstance<IMailProvider>(
   'MailProvider',
   new EtherealMailProvider(),
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
 );
