6e44a55f149323b385ea306ac6e0174d5f046a20
Maicon R. Silva <maiconrods@gmail.com>
2020-05-30 16:21:19 -0300

    (feature) List Provider Day Availability: created a feature to list only valid dates, meaning that no past dates will return available true.

--- gobarber/backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ gobarber/backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -18,12 +18,16 @@ describe('ListProvidersDayAvailability', () => {
   it('should be able to list the provider day availability', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 5, 3, 9, 0, 0),
+      date: new Date(2020, 5, 3, 13, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
       date: new Date(2020, 5, 3, 11, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 5, 3, 10, 0, 0).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -35,10 +39,12 @@ describe('ListProvidersDayAvailability', () => {
 
     expect(availability).toEqual(
       expect.arrayContaining([
-        { hour: 8, available: true },
+        { hour: 8, available: false },
         { hour: 9, available: false },
-        { hour: 10, available: true },
+        { hour: 10, available: false },
         { hour: 11, available: false },
+        { hour: 12, available: true },
+        { hour: 13, available: false },
       ]),
     );
   });
