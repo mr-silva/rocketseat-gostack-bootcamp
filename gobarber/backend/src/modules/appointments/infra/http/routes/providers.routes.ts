205cc451211220a109b0e7b28c2a2ea607be6b3e
Maicon R. Silva <maiconrods@gmail.com>
2020-06-02 13:04:36 -0300

    (feature) List Provider Availability Routes: created routes to check provider availability in the day and in the month.

--- gobarber/backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ gobarber/backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,25 @@
 import { Router } from 'express';
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderDayAvailability from '@modules/appointments/infra/http/controllers/ProviderDayAvailability';
+import ProviderMonthAvailability from '@modules/appointments/infra/http/controllers/ProviderMonthAvailability';
 import ensureAuthenticated from '@modules/users/infra/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 const providersController = new ProvidersController();
+const providerDayController = new ProviderDayAvailability();
+const providerMonthController = new ProviderMonthAvailability();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', providersController.index);
+appointmentsRouter.get(
+  '/:provider_id/day-availability',
+  providerDayController.index,
+);
+appointmentsRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthController.index,
+);
 
 export default appointmentsRouter;
