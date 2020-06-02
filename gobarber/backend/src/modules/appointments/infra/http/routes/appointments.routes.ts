a12977ae7bf0dc1e66afae6c9c6bf9a6e23e7698
Maicon R. Silva <maiconrods@gmail.com>
2020-06-02 13:48:09 -0300

    (feature) List Provider Appointments: created routes to list provider appointments in specific day.

--- gobarber/backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ gobarber/backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 import ensureAuthenticated from '@modules/users/infra/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.get('/schedule', providerAppointmentsController.index);
 
 export default appointmentsRouter;
