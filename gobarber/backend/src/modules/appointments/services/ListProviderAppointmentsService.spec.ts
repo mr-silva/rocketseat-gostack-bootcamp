a576ca5ba226ba7c95db721e57b868dc7606fb93
Maicon R. Silva <maiconrods@gmail.com>
2020-06-02 13:21:38 -0300

    (feature) List Provider Appointments: created a service to show provider the appointments in a specific day

---
+++ gobarber/backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,40 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the provider appointments on specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 5, 3, 12),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 5, 3, 16),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      day: 3,
+      month: 6,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
