b79feafea0a6c7407a52db52f271d437d45280e9
Maicon R. Silva <maiconrods@gmail.com>
2020-06-03 15:19:52 -0300

    (feature) Routes: validating routes body and params.

--- gobarber/backend/src/modules/users/infra/http/routes/profile.routes.ts
+++ gobarber/backend/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/middlewares/ensureAuthenticated';
 
@@ -10,6 +11,26 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string().when('old_password', {
+        is: Joi.exist(),
+        then: Joi.required(),
+        otherwise: Joi.forbidden(),
+      }),
+      password_confirmation: Joi.string().when('old_password', {
+        is: Joi.exist(),
+        then: Joi.required().valid(Joi.ref('password')),
+        otherwise: Joi.forbidden(),
+      }),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
--- gobarber/backend/src/shared/infra/http/server.ts
+++ gobarber/backend/src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
+app.use(errors());
+
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
@@ -33,5 +36,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(2408, () => {
+  // eslint-disable-next-line no-console
   console.log('Server started on port 2408');
 });
